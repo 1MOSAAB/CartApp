379a1bf397119c78f897ce4df865f4de0334d2da
1MOSAAB <mo.elsayad@nu.edu.eg>
2025-04-28 19:43:14 +0300

    Update 1_chatbot.py

--- pages/1_chatbot.py
+++ pages/1_chatbot.py
@@ -3,7 +3,7 @@ import streamlit as st
 from groq import Groq
 
 # Set your Groq API key
-API_KEY = st.secrets["GROQ_API_KEY"]
+API_KEY = st.secrets["api_key"]
 
 # Initialize Groq client
 client = Groq(api_key=API_KEY)
